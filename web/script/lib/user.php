019be4e5e945758104b8afa7e2833776a9ef7b03
zzh <57039182+zzhhao2008@users.noreply.github.com>
2023-12-06 21:32:49 +0800

    给泽哥加注释

--- web/script/lib/user.php
+++ web/script/lib/user.php
@@ -19,10 +19,19 @@ $emptycfg = array(
 );
 class user
 {
+    /**
+     * 获取（已登录）用户的信息
+     * @return array('username'=>"<USERID>","profile"=>"<USERPROFILE>")
+     */
     public static function read()
     {
         return array("name" => $GLOBALS['username'], "profile" => $GLOBALS['userprofile']);
     }
+    /**
+     * 查询用户信息
+     * @input username=><USERID>
+     * @return userprofile
+     */
     public static function queryUser($username)
     {
         $uprofile = DB::getdata("user/$username");
@@ -31,6 +40,11 @@ class user
         }
         return $uprofile;
     }
+    /**
+     * 查询用户昵称
+     * @input username=><USERID>,html=><是否需要使用HTML格式展示>，link=><是否需要使用链接格式展示>
+     * @return string=>用户昵称
+     */
     public static function queryUserNick($username, $html = 0, $link = 0)
     {
         $uprofile = DB::getdata("user/$username");
@@ -45,6 +59,10 @@ class user
         }
         return $uprofile['nick'];
     }
+    /**
+     * 获取用户昵称的颜色，对应Bootstrap组件颜色，输入值为用户配置文件
+     * 一般不单独使用
+     */
     public static function NickColor($user)
     {
         /*
@@ -72,11 +90,19 @@ class user
         }
         return "primary";
     }
+    /**
+     * 强制获取用户信息
+     */
     public static function queryUserAdmin($username)
     {
         $uprofile = DB::getdata("user/$username");
         return $uprofile;
     }
+    /**
+     * 登录状态检查
+     * init=>是否需要初始化
+     * @return array 用户配置文件
+     */
     public static function LoginCheck($init = 0)
     {
         $name = user::read()['name'];
@@ -88,14 +114,11 @@ class user
             return 0;
         }
     }
-    public static function saveuserchange($logining = 0)
-    {
-        if (user::LoginCheck() || $logining) {
-            DB::putdata("user/" . user::read()['name'], user::read()['profile']);
-            return 1;
-        }
-        return 0;
-    }
+    
+    /**
+     * 登录函数
+     * @return bool 是否登录成功
+     */
     public static function login($name, $pas)
     {
         $cfg = user::queryUser($name);
@@ -114,16 +137,38 @@ class user
             return 0;
         }
     }
+    /**
+     * 修改用户的配置文件 $savenow=>是否立即保存
+     */
     public static function change($key, $value, $savenow = 0): bool
     {
         $GLOBALS['userprofile'][$key] = $value;
         return $savenow ? user::saveuserchange() : 1;
     }
+    /**
+     * 给用户配置文件的某个键值添加一个新元素
+     */
     public static function change_Add($key, $value, $savenow = 0): bool
     {
         $GLOBALS['userprofile'][$key][] = $value;
         return $savenow ? user::saveuserchange() : 1;
     }
+    /**
+     * 保存对用户的更改
+     * 一般配合change()使用
+     * @return int 0=>失败 1=>成功
+     */
+    public static function saveuserchange($logining = 0)
+    {
+        if (user::LoginCheck() || $logining) {
+            DB::putdata("user/" . user::read()['name'], user::read()['profile']);
+            return 1;
+        }
+        return 0;
+    }
+    /**
+     * 读取用户的配置文件，忘记有啥用了
+     */
     public static function getMy($key)
     {
         $res = $GLOBALS['userprofile'][$key];
@@ -135,6 +180,9 @@ class user
         if (empty($y) || !is_array($cfg)) return 0;
         else return DB::putdata("user/$userid", $cfg);
     }
+    /**
+     * 获取当前用户等级
+     */
     public static function is_superuser()
     {
         return user::read()['profile']['power'] > 1;
