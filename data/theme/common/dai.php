637835ea3c884b9f8d0f43858bbf57a8de3cbd83
zzh <57039182+zzhhao2008@users.noreply.github.com>
2023-12-30 13:31:47 +0800

    针对性主题配色优化

--- data/theme/common/dai.php
+++ data/theme/common/dai.php
@@ -6,5 +6,5 @@ return array(
     'activecolor'=>'#FFFF00',
     "barcolor"=>"rgb(45,85,95)",
     "barfcolor"=>"#cde",
-    "subbackcolor"=>"rgba(170, 225, 255, 0.2)"
+    "subbackcolor"=>"rgba(170, 225, 235, 0.7)"
 );
